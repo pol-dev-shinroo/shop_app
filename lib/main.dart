5aa9189bce2d1dc8371aa7e1505d694f18219ef3
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-02 21:49:29 +0900

    themes config

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+            .copyWith(secondary: Colors.deepOrange),
+        fontFamily: "Lato",
       ),
       home: ProductsOverviewScreen(),
     );
