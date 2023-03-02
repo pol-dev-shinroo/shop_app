f4304bb6b21f583904288e19287a0a6fe72470dd
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-02 22:06:34 +0900

    adding navigation

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+
+  // const ProductDetailScreen(this.title, this.price, {super.key});
+
+  static const routeName = "/product-detail";
+
+  @override
+  Widget build(BuildContext context) {
+    final id = ModalRoute.of(context)?.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("title"),
+      ),
+    );
+  }
+}
