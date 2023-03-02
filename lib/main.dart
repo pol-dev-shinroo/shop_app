ca50e6320a8db3288974f99a83fb9e2136b7f935
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-02 18:33:25 +0900

    GridTile for mapping products

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "./screens/products_overview_screen.dart";
 
 void main() {
   runApp(const MyApp());
@@ -12,25 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shop app"),
-      ),
-      body: const Center(
-        child: Text("shop app"),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem(this.id, this.title, this.imageUrl, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(imageUrl),
+    );
+  }
+}
